84a9a4e32f6dc037a66c36a5fc494a354ca984b9
mychemromance <shubhamphoenix99@outlook.com>
2017-07-18 11:07:38 +0530

    Created new functions; defined the mem() function

--- PinnacleClub/PinnacleClub/PinnacleClub.cpp
+++ PinnacleClub/PinnacleClub/PinnacleClub.cpp
@@ -11,19 +11,85 @@ class member
 	member *link;
 
 public:
-	bool clubEmpty();
-	void getMember();
-	void newMember();
+	bool noPres();
+	bool noSec();
+	member* getMember();
+	void mem();
 	void newPresident();
 	void newSecretary();
+	void newMember();
 	void findMember();
 };
 
-member *President = NULL;
-member *Secretary = NULL;
+//these are global variables
+//Pres and Sec are analogous to heada and tail node
+member *Pres = NULL;
+member *Sec = NULL;
+int num = 0; //num stores the total number of members
+
+//returns true is the club is empty/no President
+bool member::noPres()
+{
+	return (Pres == NULL);
+}
+
+//returns true if the tail is NULL/Secretary is missing
+bool member::noSec()
+{
+	return(Sec == NULL);
+}
+
+//returns a new member*, to be used in newMember function later
+member* member::getMember()
+{
+	member* newMem = new member;
+	newMem->link = NULL;
+	return newMem;
+}
+
+void member::mem()
+{
+	if (noPres())
+	{
+		cout << "The club was empty, now creating a President...\n";
+		newPresident();
+		return;
+	}
+	else
+	{
+		cout << "Enter 1 to elect a new President\n";
+		cout << "Enter 2 to elect a new Secretary\n";
+		cout << "Enter 3 to add a new member to the club\n";
+		int c;
+		cin >> c;
+		switch (c)
+		{
+		case 1:
+		{
+			newPresident();
+			break;
+		}
+		case 2:
+		{
+			newSecretary();
+			break;
+		}
+		case 3:
+		{
+			newMember();
+			break;
+		}
+		default:
+		{
+			cout << "Invalid input\n";
+			break;
+		}
+		}
+	}
+}
 
 int main()
 {
-    return 0;
+
 }
 
