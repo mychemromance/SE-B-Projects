19f924b541ca50d1ac1c42536dbee51b43a4621c
mychemromance <shubhamphoenix99@outlook.com>
2017-07-18 11:29:01 +0530

    Updated some functions, the program is a teeny bit more complicated than a usual linked list

--- PinnacleClub/PinnacleClub/PinnacleClub.cpp
+++ PinnacleClub/PinnacleClub/PinnacleClub.cpp
@@ -3,6 +3,7 @@
 
 #include "stdafx.h"
 #include<iostream>
+#include<string>
 using namespace std;
 
 class member
@@ -27,24 +28,51 @@ member *Pres = NULL;
 member *Sec = NULL;
 int num = 0; //num stores the total number of members
 
-//returns true is the club is empty/no President
-bool member::noPres()
+bool member::noPres() //returns true is the club is empty/no President
 {
 	return (Pres == NULL);
 }
 
-//returns true if the tail is NULL/Secretary is missing
-bool member::noSec()
+bool member::noSec() //returns true if the tail is NULL/Secretary is missing
 {
 	return(Sec == NULL);
 }
 
-//returns a new member*, to be used in newMember function later
-member* member::getMember()
+member* member::getMember() //returns a new member*, to be used in newMember function later
 {
 	member* newMem = new member;
 	newMem->link = NULL;
 	return newMem;
+}
+
+void member::newPresident()
+{
+	member *newPres = getMember();
+	cout << "Enter the name of the President...\n";
+	cin >> newPres->name;
+	if (noPres())
+	{
+		newPres->link = Sec;
+		Pres = newPres;
+	}
+	else
+	{
+		newPres->link = Pres;
+		Pres = newPres;
+	}
+	num += 1;
+	return;
+}
+
+void member::newSecretary()
+{
+	member* newSec = getMember();
+	cout << "Enter the name of the secretary...\n";
+	cin >> newSec->name;
+	if (noSec())
+	{
+
+	}
 }
 
 void member::mem()
@@ -54,6 +82,11 @@ void member::mem()
 		cout << "The club was empty, now creating a President...\n";
 		newPresident();
 		return;
+	}
+	else if (noSec())
+	{
+		cout << "The club was missing a Secretary, elect one....\n";
+		newSecretary();
 	}
 	else
 	{
